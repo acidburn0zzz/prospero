480e60ec1923675f06467d80b796158b1d116e96
Lobster <lobster@riseup.net>
2019-12-04 10:21:43 +0100

    Fix Rubocop offenses

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,7 +1,11 @@
+# Base asynchronous job class
+#
+# see https://guides.rubyonrails.org/active_job_basics.html
 class ApplicationJob < ActiveJob::Base
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
 
-  # Most jobs are safe to ignore if the underlying records are no longer available
+  # Most jobs are safe to ignore if the underlying records are
+  # no longer available
   # discard_on ActiveJob::DeserializationError
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# Base model class
+#
+# see https://guides.rubyonrails.org/active_record_basics.html
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
