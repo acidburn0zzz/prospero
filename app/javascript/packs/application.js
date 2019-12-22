63ff9a120b7c10f9d576576d8b77a2c7e73e4f24
Lobster <lobster@riseup.net>
2019-12-22 22:26:24 +0100

    Add license header to application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,16 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+/**
+* Prospero
+*
+* You should have received a copy of the license along with this program.
+*
+* Licensed under
+* - GNU Affero General Public License V3
+* - CeCILL Affero compliant
+*/
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-// Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
+require("@rails/ujs").start();
+require("turbolinks").start();
 var ReactRailsUJS = require("react_ujs");
+var componentRequireContext = require.context("components", true);
+
 ReactRailsUJS.useContext(componentRequireContext);
