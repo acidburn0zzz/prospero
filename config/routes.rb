5f110765bc8b610584789e1e881f5cd95ea13b41
Lobster <lobster@riseup.net>
2019-12-24 13:27:00 +0100

    Add top-level documentation to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
+# frozen_string_literal: true
+
+# Prospero
+#
+# You should have received a copy of the license along with this program.
+#
+# Licensed under
+# - GNU Affero General Public License V3
+# - CeCILL Affero compliant
+
+# Application backend routes definitions
+#
+# See https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   root to: "home#index"
 end
