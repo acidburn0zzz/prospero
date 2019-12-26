1d7ff811e4529dff135dd7b94b1e64a2be1cbc28
Lobster <lobster@riseup.net>
2019-12-26 15:17:53 +0100

    Add PROSPERO_ prefix to environment variables

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -11,4 +11,4 @@
 echo "Initializing database..."
 source ~/.rvm/scripts/rvm
 cd /srv/prospero
-LOG_LEVEL=warn rake db:create db:migrate db:seed db:test:prepare
+PROSPERO_LOG_LEVEL=warn rake db:create db:migrate db:seed db:test:prepare
