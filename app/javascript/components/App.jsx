fb02856f0ce6f8fa7fc509ac75852de11a2475fe
Lobster <lobster@riseup.net>
2020-01-16 18:18:22 +0100

    Optimize javascript bundle pack
    Display translated GUI after lazy load i18n.

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -16,29 +16,9 @@ import React from 'react';
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import Routes from './Routes';
 
-import i18n from 'i18next';
-import XHR from 'i18next-xhr-backend';
-import { initReactI18next, withTranslation } from 'react-i18next';
-import translations from '../translations';
+import { withTranslation } from 'react-i18next';
 
 import api from '../apis/prospero';
-
-i18n
-  // load translation using xhr -> see /public/locales
-  // learn more: https://github.com/i18next/i18next-xhr-backend
-  .use(XHR)
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    fallbackLng: 'en',
-    lng: document.getElementsByTagName('html')[0].attributes.lang.value,
-    resources: {
-      en: { translation: translations['en'] },
-      fr: { translation: translations['fr'] }
-    }
-  });
 
 class App extends React.Component {
   constructor(props) {
@@ -68,20 +48,20 @@ class App extends React.Component {
           English
         </Link>
         <Link to="#"onClick={this.setLocale.bind(this, 'fr')}>Français</Link>
-          <ul>
-            <li>
-              <Link to="/">{t('menu.home')}</Link>
-            </li>
-            <li>
-              <Link to="/sample/1">{t('menu.sample', { number: 1 })}</Link>
-            </li>
-            <li>
-              <Link to="/sample/2">{t('menu.sample', { number: 2 })}</Link>
-            </li>
-            <li>
-              <Link to="/sample/3">{t('menu.sample', { number: 3 })}</Link>
-            </li>
-          </ul>
+        <ul>
+          <li>
+            <Link to="/">{t('menu.home')}</Link>
+          </li>
+          <li>
+            <Link to="/sample/1">{t('menu.sample', { number: 1 })}</Link>
+          </li>
+          <li>
+            <Link to="/sample/2">{t('menu.sample', { number: 2 })}</Link>
+          </li>
+          <li>
+            <Link to="/sample/3">{t('menu.sample', { number: 3 })}</Link>
+          </li>
+        </ul>
         <Routes />
       </Router>
     );
